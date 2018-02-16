660e18089ea2759dacca2cbadbbb11f41d2fca20
MEI CHEN <MEICHEN6@GMAIL.COM>
2018-02-16 12:07:28 -0500

    added resume

---
+++ index.js
@@ -0,0 +1,8 @@
+fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+        return data
+    }
+})
